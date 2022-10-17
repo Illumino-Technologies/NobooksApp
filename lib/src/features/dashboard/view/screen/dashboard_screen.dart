a1305d92298835ce6106d85c0a74401871ac0268
Kushimo Bashir <72566175+sublime247@users.noreply.github.com>
2022-10-17 12:41:23 +0100

    calender

--- lib/src/features/dashboard/view/screen/dashboard_screen.dart
+++ lib/src/features/dashboard/view/screen/dashboard_screen.dart
@@ -8,6 +8,7 @@ import 'package:nobook/src/utils/constants/assets.dart';
 import 'package:nobook/src/utils/extensions/size_extension.dart';
 import 'package:nobook/src/utils/sizing/sizing.dart';
 import 'package:nobook/src/utils/widgets/text/app_text.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -16,15 +17,8 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
    return Scaffold(backgroundColor: AppColors.white,
     body: Column(children: [
-      YMargin(40),
-      AppText.medium('NNKKK',),
-      Container(color: Colors.yellow,
-      height: context.height * 0.1,width: context.height* 0.1,),
-      YMargin(40),
-    SizedBox(height: 40,),
-SvgPicture.asset(Assets.logo),
-    ElevatedButton(onPressed: (){context.goNamed(AppRoutes.note);}, child: AppText.bold('nnn'))
-      
+       TableCalendar(focusedDay: DateTime.now(), firstDay: DateTime(1990), lastDay: DateTime(2050)),
+    
    ]),);
   }
 }  
