63a46980bad98a1a150cb331a3373243ec26a945
mercyjae <famakinwamercy20@gmail.com>
2022-10-19 02:01:21 -0700

    add custom font

--- lib/src/app/themes/colors.dart
+++ lib/src/app/themes/colors.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors{
-   //#79797D
-  static const Color white = Colors.white;
-
+   //#FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+  //#999EAA
+  static const Color grey100 = Color(0xFF999EAA);
+  //#0C33FF
+  static const Color blue = Color(0xFF0C33FF);
 }
--- lib/src/features/dashboard/view/screen/dashboard_screen.dart
+++ lib/src/features/dashboard/view/screen/dashboard_screen.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg/parser.dart';
-import 'package:go_router/go_router.dart';
-import 'package:nobook/src/app/navigation/app_routes.dart';
 import 'package:nobook/src/app/themes/colors.dart';
-import 'package:nobook/src/utils/constants/assets.dart';
-import 'package:nobook/src/utils/extensions/size_extension.dart';
-import 'package:nobook/src/utils/sizing/sizing.dart';
-import 'package:nobook/src/utils/widgets/text/app_text.dart';
-
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key? key}) : super(key: key);
 
@@ -16,14 +7,7 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
    return Scaffold(backgroundColor: AppColors.white,
     body: Column(children: [
-      YMargin(40),
-      AppText.medium('NNKKK',),
-      Container(color: Colors.yellow,
-      height: context.height * 0.1,width: context.height* 0.1,),
-      YMargin(40),
-    SizedBox(height: 40,),
-SvgPicture.asset(Assets.logo),
-    ElevatedButton(onPressed: (){context.goNamed(AppRoutes.note);}, child: AppText.bold('nnn'))
+ 
       
    ]),);
   }
