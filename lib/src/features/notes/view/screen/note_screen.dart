37b5e9003252e62da28ce7e901e95ac1d80fa7f4
Snow <dami.osoj@gmail.com>
2022-10-26 19:50:47 -0700

    Notes Page
    Notes page updated

--- lib/src/features/notes/view/screen/note_screen.dart
+++ lib/src/features/notes/view/screen/note_screen.dart
@@ -1,13 +1,345 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nobook/src/core/themes/color.dart';
+import 'package:nobook/src/features/notes/widgets/reused_card_widget.dart';
+//import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class NoteScreen extends StatelessWidget {
+class NoteScreen extends ConsumerStatefulWidget {
   const NoteScreen({Key? key}) : super(key: key);
+  @override
+  DashboardScreenState createState() => DashboardScreenState();
+}
 
+class DashboardScreenState extends ConsumerState<NoteScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Column(children: const []),
+      body: Center(
+        child: Container(
+          width: 758,
+          height: MediaQuery.of(context).size.height,
+          color: AppColors.mBackgroundColor,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 29),
+              child: Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  // ignore: sort_child_properties_last
+                  // ignore: prefer_const_literals_to_create_immutables
+                  Row(
+                    children: [
+                      Text(
+                        'Your Notes',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 29),
+                      ),
+                      Spacer(),
+                      MaterialButton(
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0)),
+                        height: 38,
+                        minWidth: 98,
+                        color: AppColors.blue,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Personal',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14,
+                                color: AppColors.mBackgroundColor),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'General',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Biology",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardBio(),
+                      ReusedCardBio(),
+                      ReusedCardBio(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Book Keeping",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardBk(),
+                      ReusedCardBk(),
+                      ReusedCardBk(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Chemistry",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardCh(),
+                      ReusedCardCh(),
+                      ReusedCardCh(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Civic Education",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardCv(),
+                      ReusedCardCv(),
+                      ReusedCardCv(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Economics",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardEc(),
+                      ReusedCardEc(),
+                      ReusedCardEc(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "English Language",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardEn(),
+                      ReusedCardEn(),
+                      ReusedCardEn(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Further Maths",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardFm(),
+                      ReusedCardFm(),
+                      ReusedCardFm(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Geography",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardGe(),
+                      ReusedCardGe(),
+                      ReusedCardGe(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Maths",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardMt(),
+                      ReusedCardMt(),
+                      ReusedCardMt(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Physics",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      ReusedCardPlus(),
+                      ReusedCardPh(),
+                      ReusedCardPh(),
+                      ReusedCardPh(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
