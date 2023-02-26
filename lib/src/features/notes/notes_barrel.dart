385fadd1dc65ebac91418bb84268e398c1b8d574
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-02-27 00:08:28 +0100

    intial commit

--- lib/src/features/notes/notes_barrel.dart
+++ lib/src/features/notes/notes_barrel.dart
@@ -1,3 +1,5 @@
+export 'package:nobook/src/features/notes/subfeatures/calculator/calculator_barrel.dart';
+export 'package:nobook/src/features/notes/subfeatures/drawing/drawing_barrel.dart';
 export 'package:nobook/src/features/notes/view/screen/note_page.dart';
 export 'package:nobook/src/features/notes/view/screen/note_screen.dart';
 export 'package:nobook/src/features/notes/view/widgets/note_reused_card_widget.dart';
