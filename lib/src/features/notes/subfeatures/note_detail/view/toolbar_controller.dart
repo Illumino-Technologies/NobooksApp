23b1d424d12832635527dcb02d6a5ee471974a8b
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-03-08 16:17:36 +0100

    untested implementation of undo - redo

--- lib/src/features/notes/subfeatures/note_detail/view/toolbar_controller.dart
+++ lib/src/features/notes/subfeatures/note_detail/view/toolbar_controller.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/foundation.dart';
 import 'package:nobook/src/features/notes/subfeatures/note_detail/view/base_controller.dart';
 import 'package:nobook/src/features/notes/subfeatures/note_detail/view/drawing_controller.dart';
-
-import '../../document_editing/drawing/drawing_barrel.dart';
+import 'package:nobook/src/utils/utils_barrel.dart';
 
 class ToolbarController extends ChangeNotifier
     implements DocumentEditingController {
-  final DrawingController drawingController;
+  DrawingController drawingController = DrawingController();
 
-  ToolbarController({
-    required this.drawingController,
-  }) {
-    drawingController.addListener(notifyListeners);
+  ToolbarController() {
+    drawingController.addListener(drawingControllerListener);
   }
+
+  bool _initialized = false;
+
+  bool get initialized => _initialized;
 
   @override
   Future<void> initialize() async {
     await drawingController.initialize();
+    _initialized = true;
   }
 
-  final List<Drawings> drawingsCache = [];
+  void drawingControllerListener() {}
 
-  void undo() {}
+  late final List<DocumentEditingController> cache = [];
 
-  void redo() {}
+  void setDocumentValue(DocumentEditingController controller) {
+    if (controller is DrawingController) {
+      drawingController = controller;
+      notifyListeners();
+    }
+  }
+
+  bool get canUndo => cache.isNotEmpty && cache.length > 1;
+
+  bool get canRedo =>
+      cache.isNotEmpty &&
+      cache.length > 1 &&
+      cache.contains(activeController) &&
+      !cache.isLast(activeController);
+
+  void undo() {
+    assertInitialized();
+    if (!canUndo) return;
+    setDocumentValue(cache[cache.length - 2]);
+  }
+
+  DocumentEditingController get activeController {
+    //TODO: check action stack for other controllers
+    return drawingController;
+  }
+
+  void redo() {
+    if (!canRedo) return;
+    final int currentControllerIndex = cache.indexOf(activeController);
+    setDocumentValue(cache[currentControllerIndex + 1]);
+  }
+
+  void assertInitialized() {
+    assert(initialized, 'controller must be initialized');
+  }
 
   @override
   void dispose() {
