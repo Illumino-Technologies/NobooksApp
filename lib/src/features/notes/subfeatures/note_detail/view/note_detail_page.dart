1be0e166b6204829475323f3b59314701b11ddb4
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-03-15 16:28:56 +0100

    clean up code

--- lib/src/features/notes/subfeatures/note_detail/view/note_detail_page.dart
+++ lib/src/features/notes/subfeatures/note_detail/view/note_detail_page.dart
@@ -25,6 +25,7 @@ class NoteDetailPage extends StatelessWidget {
 
 class NoteDetailPageX extends ConsumerStatefulWidget {
   final Note note;
+
   const NoteDetailPageX({
     super.key,
     required this.note,
@@ -44,173 +45,6 @@ class _NotePageState extends ConsumerState<NoteDetailPageX> {
       child: Center(
         child: Column(
           children: [
-            // FilledButton.tonal(
-            //   onPressed: () {
-            //     controller.clearDrawings();
-            //   },
-            //   child: const Text('clear'),
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     ChangeNotifierBuilder<DrawingController>(
-            //       listenable: controller,
-            //       buildWhen: (previous, next) =>
-            //           previous?.drawings != next.drawings,
-            //       builder: (_, controller) {
-            //         return ValueListenableBuilder<bool>(
-            //           valueListenable: erasingNotifier,
-            //           builder: (_, erasing, __) {
-            //             return FilledButton.tonal(
-            //               onPressed: (controller.drawings.isEmpty && !erasing)
-            //                   ? null
-            //                   : () {
-            //                       controller.changeEraseMode(EraseMode.drawing);
-            //                       controller.toggleErase();
-            //                     },
-            //               child: Text(
-            //                 erasing
-            //                     ? 'stop erasing drawing'
-            //                     : 'start erase drawing',
-            //               ),
-            //             );
-            //           },
-            //         );
-            //       },
-            //     ),
-            //     20.boxWidth,
-            //     ChangeNotifierBuilder<DrawingController>(
-            //       listenable: controller,
-            //       buildWhen: (previous, next) =>
-            //           previous?.drawings != next.drawings,
-            //       builder: (_, controller) {
-            //         return ValueListenableBuilder<bool>(
-            //           valueListenable: erasingNotifier,
-            //           builder: (_, erasing, __) {
-            //             return FilledButton.tonal(
-            //               onPressed: (controller.drawings.isEmpty && !erasing)
-            //                   ? null
-            //                   : () {
-            //                       controller.changeEraseMode(EraseMode.area);
-            //                       controller.toggleErase();
-            //                     },
-            //               child: Text(
-            //                 erasing ? 'stop erasing area' : 'start erase area',
-            //               ),
-            //             );
-            //           },
-            //         );
-            //       },
-            //     ),
-            //   ],
-            // ),
-            // 20.boxHeight,
-            // ChangeNotifierBuilder(
-            //   listenable: controller,
-            //   buildWhen: (previous, next) =>
-            //       previous?.metadataFor() == next.metadataFor(),
-            //   builder: (_, notifier) {
-            //     final DrawingMetadata metadata = notifier.metadataFor();
-            //     return Row(
-            //       mainAxisAlignment: MainAxisAlignment.center,
-            //       children: [
-            //         ...ChangeColor.values.map(
-            //           (e) => Container(
-            //             decoration: BoxDecoration(
-            //               border: metadata.color == e.color
-            //                   ? Border.all(
-            //                       color: e.color,
-            //                       width: 2,
-            //                     )
-            //                   : null,
-            //             ),
-            //             margin: EdgeInsets.only(left: 30.w),
-            //             padding: const EdgeInsets.all(5),
-            //             child: InkWell(
-            //               onTap: () => changeColor(e.color),
-            //               child: Container(
-            //                 width: 50,
-            //                 height: 50,
-            //                 color: e.color,
-            //               ),
-            //             ),
-            //           ),
-            //         )
-            //       ],
-            //     );
-            //   },
-            // ),
-            // 20.boxHeight,
-            // ChangeNotifierBuilder(
-            //   listenable: controller,
-            //   buildWhen: (previous, next) =>
-            //       previous?.drawingMode == next.drawingMode,
-            //   builder: (_, value) {
-            //     return Row(
-            //       mainAxisAlignment: MainAxisAlignment.center,
-            //       children: [
-            //         Container(
-            //           padding: const EdgeInsets.symmetric(horizontal: 4),
-            //           decoration: BoxDecoration(
-            //             borderRadius: Ui.allBorderRadius(50),
-            //             border: value.drawingMode == DrawingMode.sketch
-            //                 ? Border.all(
-            //                     color: AppColors.blue500,
-            //                     width: 2,
-            //                   )
-            //                 : null,
-            //           ),
-            //           child: FilledButton.tonal(
-            //             onPressed: () {
-            //               controller.changeDrawingMode(DrawingMode.sketch);
-            //             },
-            //             child: const Text('Sketch mode'),
-            //           ),
-            //         ),
-            //         30.boxWidth,
-            //         Container(
-            //           padding: const EdgeInsets.symmetric(horizontal: 4),
-            //           decoration: BoxDecoration(
-            //             borderRadius: Ui.allBorderRadius(50),
-            //             border: value.drawingMode == DrawingMode.shape
-            //                 ? Border.all(
-            //                     color: AppColors.blue500,
-            //                     width: 2,
-            //                   )
-            //                 : null,
-            //           ),
-            //           child: FilledButton.tonal(
-            //             onPressed: () {
-            //               controller.changeDrawingMode(DrawingMode.shape);
-            //             },
-            //             child: const Text('Shape mode'),
-            //           ),
-            //         ),
-            //       ],
-            //     );
-            //   },
-            // ),
-            // 20.boxHeight,
-            // ChangeNotifierBuilder(
-            //   listenable: controller,
-            //   buildWhen: (previous, next) => previous?.shape == next.shape,
-            //   builder: (_, value) {
-            //     return Row(
-            //       mainAxisAlignment: MainAxisAlignment.center,
-            //       children: [
-            //         ...Shape.values.map(
-            //           (e) => IconButton(
-            //             onPressed: () => controller.changeShape(e),
-            //             icon: Icon(
-            //               e.iconData,
-            //               color: value.shape == e ? AppColors.blue500 : null,
-            //             ),
-            //           ),
-            //         )
-            //       ],
-            //     );
-            //   },
-            // ),
             20.boxHeight,
             ToolBarWidget(controller: toolbarController),
             20.boxHeight,
@@ -232,16 +66,6 @@ class _NotePageState extends ConsumerState<NoteDetailPageX> {
                 );
               },
             ),
-
-            // Container(
-            //   color: AppColors.subjectOrange.withOpacity(0.4),
-            //   width: drawingBoundsHorizontal,
-            //   height: drawingBoundsVertical,
-            //   child: DrawingCanvas(
-            //     controller: controller,
-            //     size: Size(drawingBoundsHorizontal, drawingBoundsVertical),
-            //   ),
-            // ),
           ],
         ),
       ),
