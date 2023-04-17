1665560e0bb36e8ef351ec90e5aafd1b128e00f2
Kushimo Bashir <kushimobashir2000@gmail.com>
2023-04-17 22:05:30 +0100

    implented recorded subject

--- lib/src/features/records/ui/record_page/view/record_page.dart
+++ lib/src/features/records/ui/record_page/view/record_page.dart
@@ -1,39 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nobook/src/global/domain/fakes/subject/fake_subjects.dart';
+import 'package:nobook/src/global/ui/ui_barrel.dart';
+import 'package:nobook/src/utils/function/extensions/extensions.dart';
+
 class RecordPage extends ConsumerStatefulWidget {
-  const RecordPage({super.key});
+  const RecordPage({super.key, this.styles});
+  final TextStyles? styles;
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _RecordPageState();
 }
 
 class _RecordPageState extends ConsumerState<RecordPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Record'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text('Record Page'),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10.w,
+                vertical: 10.h,
+              ),
+              child: Column(
+                children: [
+                  Text('All Subjects Taken Up',
+                      style: TextStyles.headline1.withSize(24)),
+                  SizedBox(
+                    height: context.screenHeight * 0.85,
+                    width: 320.w,
+                    child: ListView.builder(
+                      itemCount: FakeSubjects.subjects.length,
+                      itemBuilder: (context, index) => ExpansionTile(
+                        leading: SubjectWidget(
+                          subject: FakeSubjects.subjects[index],
+                          boxSize: 40.r,
+                          fontSize: 25.sp,
+                        ),
+                        title: Text(
+                          FakeSubjects.subjects[index].name,
+                          style: TextStyles.paragraph1.asSemibold,
+                        ),
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Continous Assessment',
+                                  style: TextStyles.headline1.withSize(20)),
+                              SizedBox(
+                                height: 20.h,
+                                width: 20.w,
+                                child: Padding(
+                                    padding: EdgeInsets.all(5.w),
+                                    child: const Text('40')),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Examination',
+                                  style: TextStyles.headline1.withSize(20)),
+                              SizedBox(
+                                height: 20.h,
+                                width: 20.w,
+                                child: Padding(
+                                    padding: EdgeInsets.all(5.w),
+                                    child: const Text('40'),),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.green,
+          )
+        ],
       ),
     );
   }
