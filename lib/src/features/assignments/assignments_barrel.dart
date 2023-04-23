c079fb74545ed5fba7a8a726829324d4fa2f331e
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-04-23 22:04:51 +0100

    add assignments folder structure

--- lib/src/features/assignments/assignments_barrel.dart
+++ lib/src/features/assignments/assignments_barrel.dart
@@ -1,1 +1,2 @@
+export 'package:nobook/src/features/assignments/subfeatures/assignments_list/ui/view/assignments_page.dart';
 export 'package:nobook/src/features/assignments/subfeatures/assignment/assignment_barrel.dart';
---
+++ lib/src/features/assignments/subfeatures/assignments_list/ui/view/assignments_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AssignmentsPage extends StatelessWidget {
+  const AssignmentsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
