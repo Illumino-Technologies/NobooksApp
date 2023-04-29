8dc6f8b8562a53ef9b56ba14502c9b36ed99f9b6
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-04-29 18:36:29 +0100

    fix assignment state development blocker

--- lib/src/features/assignments/subfeatures/assignment/ui/state_mgmt/states/assignment_state.dart
+++ lib/src/features/assignments/subfeatures/assignment/ui/state_mgmt/states/assignment_state.dart
@@ -1,43 +1,44 @@
-// part of '../assignment_state_notifier.dart';
-//
-// class AssignmentState {
-//   final List<ToolbarController> questionControllers;
-//   final List<ToolbarController> answerControllers;
-//   final Assignment? _assignment;
-//
-//   const AssignmentState({
-//     required this.questionControllers,
-//     required this.answerControllers,
-//     required Assignment? assignment,
-//   }) : _assignment = assignment;
-//
-//   Assignment get assignment => _assignment!;
-//
-//   void seControllersOffOfAssignment() {
-//     questionControllers.clear();
-//     answerControllers.clear();
-//
-//     ToolbarController(
-//       note: Note(
-//         id: id,
-//         subject: subject,
-//         topic: topic,
-//         noteBody: noteBody,
-//         createdAt: createdAt,
-//         updatedAt: updatedAt,
-//       ),
-//     );
-//   }
-//
-//   AssignmentState copyWith({
-//     List<ToolbarController>? questionControllers,
-//     List<ToolbarController>? answerControllers,
-//     Assignment? assignment,
-//   }) {
-//     return AssignmentState(
-//       questionControllers: questionControllers ?? this.questionControllers,
-//       answerControllers: answerControllers ?? this.answerControllers,
-//       assignment: assignment ?? this.assignment,
-//     );
-//   }
-// }
+part of '../assignment_state_notifier.dart';
+
+class AssignmentState {
+  final List<NoteDocumentController> questionControllers;
+  final List<NoteDocumentController> answerControllers;
+  final Assignment? _assignment;
+
+  const AssignmentState({
+    required this.questionControllers,
+    required this.answerControllers,
+    required Assignment? assignment,
+  }) : _assignment = assignment;
+
+  Assignment get assignment => _assignment!;
+
+  void seControllersOffOfAssignment() {
+    if (_assignment == null) return;
+    questionControllers.clear();
+    answerControllers.clear();
+    for (final AssignmentOperation operation in _assignment!.questions) {
+      questionControllers.add(
+        NoteDocumentController(noteDocument: operation.content)..initialize(),
+      );
+    }
+    if (_assignment?.answers == null) return;
+    for (final AssignmentOperation operation in assignment.answers!) {
+      answerControllers.add(
+        NoteDocumentController(noteDocument: operation.content)..initialize(),
+      );
+    }
+  }
+
+  AssignmentState copyWith({
+    List<NoteDocumentController>? questionControllers,
+    List<NoteDocumentController>? answerControllers,
+    Assignment? assignment,
+  }) {
+    return AssignmentState(
+      questionControllers: questionControllers ?? this.questionControllers,
+      answerControllers: answerControllers ?? this.answerControllers,
+      assignment: assignment ?? this.assignment,
+    );
+  }
+}
