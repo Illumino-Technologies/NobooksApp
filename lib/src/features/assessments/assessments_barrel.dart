00a93f7a92b7c4c0f2b11527c33d8e7eb0216ff1
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-05-07 01:52:37 +0100

    clean up code

--- lib/src/features/assessments/assessments_barrel.dart
+++ lib/src/features/assessments/assessments_barrel.dart
@@ -3,4 +3,5 @@ export 'package:nobook/src/features/assessments/domain/fake_data/assessment/fake
 export 'package:nobook/src/features/assessments/domain/models/assessment/assessment.dart';
 export 'package:nobook/src/features/assessments/domain/models/assessment_type/assessment_type.dart';
 export 'package:nobook/src/features/assessments/subfeatures/assessment_detail/ui/assessment_detail_page.dart';
+export 'package:nobook/src/features/assessments/subfeatures/assessment_listings/data/repository/assessment_repository.dart';
 export 'package:nobook/src/features/assessments/subfeatures/assessment_listings/ui/presentation/assessments_page.dart';
