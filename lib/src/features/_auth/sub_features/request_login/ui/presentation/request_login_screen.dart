929d65926e6cb4bf9cc2c041634700799e518c7c
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-06-07 00:45:42 +0100

    start request login details screen

--- lib/src/features/_auth/sub_features/request_login/ui/presentation/request_login_screen.dart
+++ lib/src/features/_auth/sub_features/request_login/ui/presentation/request_login_screen.dart
@@ -5,6 +5,8 @@ class RequestLoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: const Placeholder(),
+    );
   }
 }
